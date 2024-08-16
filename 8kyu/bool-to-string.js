fe7222b82f3f57e721240bec8de3d9bdedb04a82
Billy King <74845157+verike1996@users.noreply.github.com>
2024-08-16 09:08:22 -0400

    solved bool-to-string.js

---
+++ 8kyu/bool-to-string.js
@@ -0,0 +1,5 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// My Solution
+
+let boolToWord = bool => bool === true ? 'Yes' : 'No'
