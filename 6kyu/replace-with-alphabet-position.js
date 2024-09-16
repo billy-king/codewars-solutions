0b6be08709b0938442f389265fce35eea20f349f
Billy King <billyjack30@gmail.com>
2024-09-16 16:33:08 -0400

    solved narcissistic-numbers.js and replace-with-alphabet-position.js

---
+++ 6kyu/replace-with-alphabet-position.js
@@ -0,0 +1,27 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// Input = "The sunset sets at twelve o' clock."
+// Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+// My Solution
+
+function alphabetPosition(text) {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+    return text.toLowerCase()
+      .split('')
+      .filter(e => alphabet.includes(e))
+      .map(e => {
+        if (alphabet.includes( e)) {
+          return alphabet.indexOf(e) + 1
+        }
+      })
+      .join(' ');
+}
